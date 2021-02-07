b262e14df2ad2bcd8d992566b6ba47a382f7a149
Gabriel Enrico <enricorocha1@hotmail.com>
2021-02-07 16:46:02 -0300

    Better Tray Organization

--- DropMeter/App.xaml.cs
+++ DropMeter/App.xaml.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 using CefSharp;
 using CefSharp.Wpf;
 using DropMeter.CEF;
 using DropMeter.PluginMgr;
+using Application = System.Windows.Application;
 
 namespace DropMeter
 {
@@ -21,6 +25,9 @@ namespace DropMeter
     {
         internal static Dictionary<string, HTMLWidget> OpenWidgets = new Dictionary<string, HTMLWidget>();
         internal static List<string> AvailableWidgets = new List<string>();
+
+        internal static string WidgetsBase = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "widgets");
+
         [STAThread]
         public static void Main()
         {
@@ -43,7 +50,7 @@ namespace DropMeter
             settings.RegisterScheme(new CefCustomScheme
             {
                 SchemeName = LocalFileHandlerFactory.SchemeName,
-                SchemeHandlerFactory = new LocalFileHandlerFactory(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "widgets")), //TODO
+                SchemeHandlerFactory = new LocalFileHandlerFactory(WidgetsBase), //TODO
                 IsCorsEnabled = true,
                 IsSecure = true,
                 IsCSPBypassing = true,
@@ -61,13 +68,9 @@ namespace DropMeter
         private System.Windows.Forms.NotifyIcon _notifyIcon;
         private bool _isExit;
 
-        protected override void OnStartup(StartupEventArgs e)
+        public void LoadWidgets()
         {
-            base.OnStartup(e);
-            MainWindow = new MainWindow();
-            MainWindow.Closing += MainWindow_Closing;
-            var widgetDir = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "widgets");
-            foreach (var widget in Directory.EnumerateDirectories(widgetDir))
+            foreach (var widget in Directory.EnumerateDirectories(WidgetsBase))
             {
                 if (File.Exists(Path.Combine(widget, "index.html")))
                 {
@@ -82,6 +85,14 @@ namespace DropMeter
                     Console.WriteLine($"Widget at {widget} has no Entrypoint! Skipping.");
                 }
             }
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+            
+            LoadWidgets();
 
             _notifyIcon = new System.Windows.Forms.NotifyIcon();
             _notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
@@ -96,12 +107,46 @@ namespace DropMeter
             _notifyIcon.ContextMenuStrip =
                 new System.Windows.Forms.ContextMenuStrip();
             _notifyIcon.ContextMenuStrip.Items.Add("Manage DropMeter").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("-");
+            if (OpenWidgets.Count == 0)
+            {
+                _notifyIcon.ContextMenuStrip.Items.Add("No Widgets Loaded");
+            }
             foreach (var widget in OpenWidgets)
             {
-                _notifyIcon.ContextMenuStrip.Items.Add("Manage: " + widget.Key).Click +=
-                    (s, e) => widget.Value.EnterMoveMode();
-            }
+                ToolStripMenuItem menu;
+                //_notifyIcon.ContextMenuStrip.Items.Add("Manage: " + widget.Key).Click += (s, e) => widget.Value.EnterMoveMode();
+                var iconPath = Path.Combine(WidgetsBase, widget.Key, "favicon.ico");
+                var menuTitle = $"Widget: ${widget.Key}";
+                if (File.Exists(iconPath))
+                {
+                    Icon icoFile = new Icon(iconPath);
+                    Image favicon = icoFile.ToBitmap();
 
+                    menu = new ToolStripMenuItem(menuTitle, favicon);
+                }
+                else menu = new ToolStripMenuItem(menuTitle);
+                menu.DropDown = new ToolStripDropDown();
+
+                menu.DropDown.Items.Add("Enter Management Mode").Click += (s, e) => widget.Value.EnterMoveMode();
+                menu.DropDown.Items.Add("Unload").Click += (s, e) => widget.Value.Close();
+
+
+                _notifyIcon.ContextMenuStrip.Items.Add(menu);
+
+
+            }
+            _notifyIcon.ContextMenuStrip.Items.Add("-");
+            _notifyIcon.ContextMenuStrip.Items.Add("Reload Widgets").Click += (s, e) =>
+            {
+                foreach (var widget in OpenWidgets)
+                {
+                    widget.Value.Close();
+                    
+                }
+                OpenWidgets = new Dictionary<string, HTMLWidget>();
+                LoadWidgets();
+            };
             _notifyIcon.ContextMenuStrip.Items.Add("Shutdown").Click += (s, e) => ExitApplication();
         }
 
@@ -138,6 +183,10 @@ namespace DropMeter
                 MainWindow.Hide(); // A hidden window can be shown again, a closed one not
             }
         }
+
+
+        
     }
+
 }
 
